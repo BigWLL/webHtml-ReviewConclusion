53fd3ff76068c3cf341fc57d3d0c958bdf897e3e
王立玲 <shizizuoxiaomifeng@126.com>
2021-02-25 09:22:30 +0800

    add

---
+++ typescript/index.ts
@@ -0,0 +1,10 @@
+var str:string = 'hello';
+
+// 布尔类型 boolean  : true  false
+var flag:boolean = true;
+flag = false;
+
+// 数字类型 （number）
+var num:number = 12;
+num = 23;
+
---
+++ typescript/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var str = 'hello';
+// 布尔类型 boolean  : true  false
+var flag = true;
+flag = false;
+// 数字类型 （number）
+var num = 12;
+num = 23;
